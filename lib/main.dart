b053b391270d80bfb965f90876cea03ff2378d94
Harsha Padma R <padmaharsha02@gmail.com>
2024-04-18 14:41:07 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:libnotif/firebase_options.dart';
 import 'login.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+Future<void> main() async {
+  
   runApp(MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 }
 
 class MyApp extends StatelessWidget {
